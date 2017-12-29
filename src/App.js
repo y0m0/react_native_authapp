916b22ad8219bb3d416f87754b801b7e36998228
y0m0 <lixi.daniele@gmail.com>
2017-12-29 19:52:35 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>My new App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
