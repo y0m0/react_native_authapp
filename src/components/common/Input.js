7817fe8c55d26a7d68424aa66cf7a655bea7eab7
y0m0 <lixi.daniele@gmail.com>
2017-12-31 15:43:30 +0100

    Add a Input component which is a wrapper around TextInput

---
+++ src/components/common/Input.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TextInput, Text, View } from 'react-native';
+
+const Input = ({ label, value, onChangeText }) => {
+  return (
+    <View>
+      <Text>{label}</Text>
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        style={{ height: 20, width: 100 }}
+      />
+    </View>
+  );
+}
+
+const styles = {
+
+}
+
+export { Input };
