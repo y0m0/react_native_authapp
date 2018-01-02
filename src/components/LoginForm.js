5f628911db4d16023c5af3839df54d9030cd1fd6
y0m0 <lixi.daniele@gmail.com>
2018-01-02 15:05:05 +0100

    Conditionally show the spinner instead of the login button when trying to authenticate

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '', error: '' };
+  state = { email: '', password: '', error: '', loading: false };
 
   onButtonPress() {
     const { email, password } = this.state;
 
    // reset the error state everytime the button is pressed
-    this.setState({ error: '' });
+    this.setState({ error: '', loading: true });
 
     firebase.auth().signInWithEmailAndPassword(email, password)
       .catch(() => {
@@ -19,6 +19,17 @@ class LoginForm extends Component {
             this.setState({error: 'Authentication Failed.'});
           });
       });
+  };
+
+  renderButton () {
+     if (this.state.loading) return <Spinner size="small" />;
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    );
+
   };
 
   render() {
@@ -48,9 +59,7 @@ class LoginForm extends Component {
         </Text>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
